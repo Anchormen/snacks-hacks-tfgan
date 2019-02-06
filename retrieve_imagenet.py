52c690f5e082203d99aff0efc1992a895dbf95dd
Ron Dotsch <r.dotsch@anchormen.nl>
2019-02-06 14:05:24 +0100

    Web scraper now also retrieves imagenet wordnet mappings

--- retrieve_imagenet.py
+++ retrieve_imagenet.py
@@ -3,6 +3,8 @@ import requests
 import tarfile
 
 
+# Retrieve and untar imagenet urls
+
 url = 'http://image-net.org/imagenet_data/urls/imagenet_fall11_urls.tgz'
 tarname= "imagenet_fall11_urls.tar"
 
@@ -27,3 +29,16 @@ finally :
     print("Extracting ImageNet urls...")
     tar.extractall()
     tar.close()
+
+# Retrieve imagenet - wordnet mapping
+
+url = 'http://image-net.org/archive/words.txt'
+mappingname = 'mapping.txt'
+print("Downloading ImageNet - WordNet mapping...")
+
+response = requests.get(url, stream=True)
+total_size = int(response.headers.get('content-length', 0))
+block_size = 1024
+with open(mappingname, "wb") as handle:
+    for data in tqdm(response.iter_content(block_size), total = total_size // block_size, unit ='KB'):
+        handle.write(data)
