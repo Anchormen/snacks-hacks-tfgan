e9d34f4b6aca3350cd2dd6e88d9d3d5ba6fd7d1a
Jeroen Vlek <j.vlek@anchormen.nl>
2019-02-11 08:46:33 +0100

    added some flags and turned leaky_relu into a proper def

--- pokemon_generator.py
+++ pokemon_generator.py
@@ -7,76 +7,33 @@ import os
 from absl import flags
 from absl import logging
 import tensorflow as tf
+
 tfgan = tf.contrib.gan
 layers = tf.contrib.layers
-from tf_gan_research_deps import  data_provider
+from tf_gan_research_deps import data_provider
 from plot_gan_image_hook import PlotGanImageHook
-
 
 flags.DEFINE_integer('batch_size', 32, 'The number of images in each batch.')
 
 flags.DEFINE_string('train_log_dir', '/tmp/pokemon/',
                     'Directory where to write event logs.')
 
-flags.DEFINE_string('dataset_dir', '/tmp/mnist_data', 'Location of data.')
+flags.DEFINE_string('dataset_dir', '/tmp/pokemon_data', 'Location of data.')
 
 flags.DEFINE_integer('max_number_of_steps', 20000,
                      'The maximum number of gradient steps.')
 
-flags.DEFINE_string(
-    'gan_type', 'unconditional',
-    'Either `unconditional`, `conditional`, or `infogan`.')
-
 flags.DEFINE_integer(
     'grid_size', 5, 'Grid size for image visualization.')
-
 
 flags.DEFINE_integer(
     'noise_dims', 64, 'Dimensions of the generator noise vector.')
 
 FLAGS = flags.FLAGS
 
-_leaky_relu = lambda x: tf.nn.leaky_relu(x, alpha=0.01)
 
-def _generator_helper(
-        noise, is_conditional, one_hot_labels, weight_decay, is_training):
-    """Core MNIST generator.
-
-    This function is reused between the different GAN modes (unconditional,
-    conditional, etc).
-
-    Args:
-      noise: A 2D Tensor of shape [batch size, noise dim].
-      is_conditional: Whether to condition on labels.
-      one_hot_labels: Optional labels for conditioning.
-      weight_decay: The value of the l2 weight decay.
-      is_training: If `True`, batch norm uses batch statistics. If `False`, batch
-        norm uses the exponential moving average collected from population
-        statistics.
-
-    Returns:
-      A generated image in the range [-1, 1].
-    """
-    with tf.contrib.framework.arg_scope(
-            [layers.fully_connected, layers.conv2d_transpose],
-            activation_fn=tf.nn.relu, normalizer_fn=layers.batch_norm,
-            weights_regularizer=layers.l2_regularizer(weight_decay)):
-        with tf.contrib.framework.arg_scope(
-                [layers.batch_norm], is_training=is_training):
-            net = layers.fully_connected(noise, 1024)
-            if is_conditional:
-                net = tfgan.features.condition_tensor_from_onehot(net, one_hot_labels)
-            net = layers.fully_connected(net, 7 * 7 * 128)
-            net = tf.reshape(net, [-1, 7, 7, 128])
-            net = layers.conv2d_transpose(net, 64, [4, 4], stride=2)
-            net = layers.conv2d_transpose(net, 32, [4, 4], stride=2)
-            # Make sure that generator output is in the same range as `inputs`
-            # ie [-1, 1].
-            net = layers.conv2d(
-                net, 1, [4, 4], normalizer_fn=None, activation_fn=tf.tanh)
-
-            return net
-
+def _leaky_relu(x):
+    return tf.nn.leaky_relu(x, alpha=0.01)
 
 def unconditional_generator(noise, weight_decay=2.5e-5, is_training=True):
     """Generator to produce unconditional MNIST images.
@@ -91,39 +48,27 @@ def unconditional_generator(noise, weight_decay=2.5e-5, is_training=True):
     Returns:
       A generated image in the range [-1, 1].
     """
-    return _generator_helper(noise, False, None, weight_decay, is_training)
 
-def _discriminator_helper(img, is_conditional, one_hot_labels, weight_decay):
-    """Core MNIST discriminator.
-
-    This function is reused between the different GAN modes (unconditional,
-    conditional, etc).
-
-    Args:
-      img: Real or generated MNIST digits. Should be in the range [-1, 1].
-      is_conditional: Whether to condition on labels.
-      one_hot_labels: Labels to optionally condition the network on.
-      weight_decay: The L2 weight decay.
-
-    Returns:
-      Final fully connected discriminator layer. [batch_size, 1024].
-    """
     with tf.contrib.framework.arg_scope(
-            [layers.conv2d, layers.fully_connected],
-            activation_fn=_leaky_relu, normalizer_fn=None,
-            weights_regularizer=layers.l2_regularizer(weight_decay),
-            biases_regularizer=layers.l2_regularizer(weight_decay)):
-        net = layers.conv2d(img, 64, [4, 4], stride=2)
-        net = layers.conv2d(net, 128, [4, 4], stride=2)
-        net = layers.flatten(net)
-        if is_conditional:
-            net = tfgan.features.condition_tensor_from_onehot(net, one_hot_labels)
-        net = layers.fully_connected(net, 1024, normalizer_fn=layers.layer_norm)
+            [layers.fully_connected, layers.conv2d_transpose],
+            activation_fn=tf.nn.relu, normalizer_fn=layers.batch_norm,
+            weights_regularizer=layers.l2_regularizer(weight_decay)):
+        with tf.contrib.framework.arg_scope(
+                [layers.batch_norm], is_training=is_training):
+            net = layers.fully_connected(noise, 1024)
+            net = layers.fully_connected(net, 7 * 7 * 128)
+            net = tf.reshape(net, [-1, 7, 7, 128])
+            net = layers.conv2d_transpose(net, 64, [4, 4], stride=2)
+            net = layers.conv2d_transpose(net, 32, [4, 4], stride=2)
+            # Make sure that generator output is in the same range as `inputs`
+            # ie [-1, 1].
+            net = layers.conv2d(
+                net, 1, [4, 4], normalizer_fn=None, activation_fn=tf.tanh)
 
-        return net
+            return net
 
 
-def unconditional_discriminator(img, unused_conditioning, weight_decay=2.5e-5):
+def unconditional_discriminator(img, weight_decay=2.5e-5):
     """Discriminator network on unconditional MNIST digits.
 
     Args:
@@ -137,8 +82,18 @@ def unconditional_discriminator(img, unused_conditioning, weight_decay=2.5e-5):
     Returns:
       Logits for the probability that the image is real.
     """
-    net = _discriminator_helper(img, False, None, weight_decay)
+    with tf.contrib.framework.arg_scope(
+            [layers.conv2d, layers.fully_connected],
+            activation_fn=_leaky_relu, normalizer_fn=None,
+            weights_regularizer=layers.l2_regularizer(weight_decay),
+            biases_regularizer=layers.l2_regularizer(weight_decay)):
+        net = layers.conv2d(img, 64, [4, 4], stride=2)
+        net = layers.conv2d(net, 128, [4, 4], stride=2)
+        net = layers.flatten(net)
+        net = layers.fully_connected(net, 1024, normalizer_fn=layers.layer_norm)
+
     return layers.linear(net, 1)
+
 
 def _learning_rate(gan_type):
     # First is generator learning rate, second is discriminator learning rate.
@@ -158,7 +113,7 @@ def main(_):
             images, one_hot_labels, _ = data_provider.provide_data(
                 'train', FLAGS.batch_size, FLAGS.dataset_dir, num_threads=4)
 
-    generator_fn=unconditional_generator
+    generator_fn = unconditional_generator
     noise_fn = tf.random_normal(
         [FLAGS.batch_size, FLAGS.noise_dims])
     gan_model = tfgan.gan_model(
